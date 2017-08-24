678e45fb122970f5a869a611d86a6f80d7a540c0
Paweł Sporysz <pawel.s@untitledkingdom.co>
2017-08-24 16:41:27 +0200

    Initial project

---
+++ BlueBell/BlueBell.h
@@ -0,0 +1,19 @@
+//
+//  BlueBell.h
+//  BlueBell
+//
+//  Created by Paweł Sporysz on 24.08.2017.
+//  Copyright © 2017 Paweł Sporysz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BlueBell.
+FOUNDATION_EXPORT double BlueBellVersionNumber;
+
+//! Project version string for BlueBell.
+FOUNDATION_EXPORT const unsigned char BlueBellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlueBell/PublicHeader.h>
+
+
