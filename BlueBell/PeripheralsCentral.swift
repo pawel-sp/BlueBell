f12f15da57fa2cc9ed43365cb38801233f29f4c0
Paweł Sporysz <pawel.s@untitledkingdom.co>
2017-08-25 10:30:06 +0200

    Added client example project

---
+++ BlueBell/PeripheralsCentral.swift
@@ -0,0 +1,15 @@
+//
+//  PeripheralsManager.swift
+//  BlueBell
+//
+//  Created by Paweł Sporysz on 25.08.2017.
+//  Copyright © 2017 Paweł Sporysz. All rights reserved.
+//
+
+import Foundation
+
+class PeripheralCentral {
+    
+    
+    
+}
---
+++ iOS-Client-Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Client-Example
+//
+//  Created by Paweł Sporysz on 25.08.2017.
+//  Copyright © 2017 Paweł Sporysz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
