78b9166a2e2297a6187927ac7fcb1c82fc5d2356
Paweł Sporysz <pawel.s@untitledkingdom.co>
2017-09-06 10:41:14 +0200

    Minor changes

--- BlueBell/PeripheralClient.swift
+++ BlueBell/PeripheralClient.swift
@@ -61,27 +61,22 @@ class PeripheralClient {
     // MARK: - Actions
     
     func perform<ValueType>(command: PeripheralCommand<ValueType>, completion: ResultCompletion<ValueType>? = nil) {
-        
         guard peripheral.state == .connected else {
             completion?(.error(ClientError.deviceNotConnected(peripheral.state)))
             return
         }
-        
         guard let cbCharacteristic = characteristics.first(for: command.operation.characteristic) else {
             completion?(.error(ClientError.incorrectCharacteristicForOperation(command.operation.characteristic.uuidString)))
             return
         }
-        
         if let expectedCharacteristic = command.expectation?.characteristic, characteristics.first(for: expectedCharacteristic) == nil {
             completion?(.error(ClientError.incorrectCharacteristicForExpectation(expectedCharacteristic.uuidString)))
             return
         }
-        
         if let completion = completion, (command.expectation == nil || command.expectation?.isEmpty == true) {
             completion(.error(ClientError.missingExpectation))
             return
         }
-        
         switch command.operation {
             case .read(_):
                 peripheral.readValue(for: cbCharacteristic)
@@ -89,26 +84,21 @@ class PeripheralClient {
                 let data = command.transformer.transform(valueToData: value)
                 peripheral.writeValue(data, for: cbCharacteristic, type: .withResponse)
         }
-        
         if let completion = completion {
             let request = CommandRequest(command: command, completion: completion)
             commandRequestQueue.add(request: request)
         }
-    
     }
     
     func register<ValueType>(subscription: PeripheralSubscription<ValueType>, update: @escaping ResultCompletion<ValueType>) {
-        
         if peripheral.state != .connected {
             update(.error(ClientError.deviceNotConnected(peripheral.state)))
             return
         }
-        
         guard let cbCharacteristic = characteristics.first(for: subscription.characteristic) else {
             update(Result.error(ClientError.incorrectCharacteristicForOperation(subscription.characteristic.uuidString)))
             return
         }
-        
         let request = SubscriptionRequest(subscription: subscription, update: update)
         subscriptionRequestQueue.add(request: request)
         peripheral.setNotifyValue(true, for: cbCharacteristic)
@@ -125,12 +115,11 @@ class PeripheralClient {
     // MARK: - Private
     
     private func preparedPeripheralDelegate() -> Delegate {
-        //timeouts?, loosing connection?
+        
         let didUpdateAction: Completion<CBCharacteristic> = { characteristic, error in
             if error != nil {
                 let request = self.commandRequestQueue.removeFirstRequst(for: characteristic)
                 request?.finish(error: error)
-                // anulowac pozostale requesty na tej charakterystyce? bo zrobi sie rozjazd
             } else {
                 // request
                 guard let data = characteristic.value, let request = self.commandRequestQueue.firstRequest(for: characteristic) else { return }
@@ -148,6 +137,7 @@ class PeripheralClient {
                 let request = self.commandRequestQueue.removeFirstRequst(for: characteristic)
                 request?.finish(error: error)
             } else {
+                // request
                 guard let data = characteristic.value, let request = self.commandRequestQueue.firstRequest(for: characteristic) else { return }
                 if request.process(write: data) == .finished {
                     request.finish(error: nil)
